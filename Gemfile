a7e82a0e61e5765866eb9af3dd04501250e058ee
Stuart Ingram <ingrams2@upmc.com>
2016-05-05 11:51:18 -0400

    Cleanup to first approach.  Concerns over this approach with large queues remain.

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source 'https://rubygems.org'
 
 #gem 'activesupport', '3.2.14',  :platform => :jruby
 gem 'ruby-progressbar'
-gem 'thread_safe'
-#gem 'concurrent-ruby', require: 'concurrent'
+#gem 'thread_safe'
+gem 'concurrent-ruby', require: 'concurrent'
--- dl.rb
+++ dl.rb
@@ -1,16 +1,22 @@
 require 'open-uri'
 require 'json'
-require 'thread_safe'
+require 'concurrent'
+require 'pp'
 
 require 'logger'
 require  'ruby-progressbar'
 
+# http://www.nurkiewicz.com/2014/11/executorservice-10-tips-and-tricks.html
+# http://ericlondon.com/2014/04/02/jruby-thread-pool-concurrency-example-to-pull-data-from-elasticsearch.html
+# https://github.com/jruby/jruby/wiki/Concurrency-in-jruby#thread_safety
+# https://github.com/ruby-concurrency/concurrent-ruby
+
 java_import java.util.concurrent.Executors
 
-class SourceList
+class PersonList
 
   def self.get(size = 1000)
-    list = ThreadSafe::Array.new
+    list = Concurrent::Array.new
     list += [*1..size]
     list
   end
@@ -21,63 +27,73 @@ java.util.concurrent.ThreadPoolExecutor.class_eval do
   java_alias :submit, :submit, [java.util.concurrent.Callable.java_class]
 end
 
-class Downloader
+class PeopleDownloader
+
+  attr_accessor  :person_count, :people_progressbar, :documents
+
   def initialize
     @log = Logger.new STDOUT
-    @sourcelist = SourceList.get
-    @progressbar = ProgressBar.create(total: @sourcelist.size,
-                                      format: "%t: |%B| %c/%C %E %a",
-                                      title: "Things"
-                                     )
+    @people = PersonList.get
+    @person_count = @people.size
+    @people_progressbar = ProgressBar.create(total: person_count,
+                                             format: "%t: |%B| %c/%C %E %a",
+                                             title: "People"
+                                            )
+    @documents = Concurrent::Array.new
   end
 
-  def progressbar
-    @progressbar
-  end
-
-  # inner class used to interface with Callable
-  class Worker
-    include java.util.concurrent.Callable
-
-    def initialize(master, id)
-      @id = id
-      @master = master
-    end
-
-    def call
-      process_next(@id)
-    end
-
-    def process_next(id)
-      return false if id.nil?
-      @master.progressbar.increment
-      subcount = Random.new.rand(100)
-      @master.progressbar.log "Retrieving #{subcount} sub things"
-#      puts id
-      sleep(1)
-      #    @log.debug "processing index: #{id}"
-      # Subcount stuff here
-      return true
-    end
-
-  end
 
 
   # main method to define a fixed thread pool and process all indexes
   def work
     executor = java.util.concurrent.Executors::newFixedThreadPool 15
-#    Signal.trap('SIGINT') { executor.shutdown }
-    while @sourcelist.size > 0
-      executor.submit( Worker.new(self, @sourcelist.pop) )
+    # Pump the requests into the queue for the thread pool to act on.
+    while !@people.empty?
+      executor.submit( PersonWorker.new(self, @people.pop) )
       break if executor.is_shutdown
     end
+    # Block until queue is drained
+    while !executor.queue.empty? do
+      sleep 1
+    end
     executor.shutdown
+    @people_progressbar.refresh
+
+  end
+
+end
+
+# inner class used to interface with Callable
+class PersonWorker
+  include java.util.concurrent.Callable
+
+  def initialize(master, id)
+    @master = master
+    @id = id
+  end
+
+  def call
+    process_next(@id)
+  end
+
+  def process_next(id)
+    # Process atomic request
+    return false if id.nil?
+    @master.people_progressbar.increment
+    document_count = Random.new.rand(100)
+    @master.documents += [*1..document_count]
+    sleep 0.2
+    # Subcount stuff here
+    return true
   end
 
 end
 
 
-
-# run it:
-data = Downloader.new
+data = PeopleDownloader.new
 data.work
+
+puts
+puts "------------"
+puts "People - #{data.person_count}"
+puts "Documents - #{data.documents.size}"
